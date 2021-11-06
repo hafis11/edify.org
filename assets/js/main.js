e0315c99d584e5a1faff99d9245c9890fb60283e
Hafis muhammed <frontend.developer.g1b@t-dna.in>
2021-11-06 09:11:14 +0530

    edify.org first commit

---
+++ assets/js/main.js
@@ -0,0 +1,123 @@
+jQuery(function ($) {
+    "use strict";
+
+    var footerHeight = $(".main-wrapper").height();
+    $(".main-wrapper").css('margin-bottom', footerHeight + 'px');
+
+    /*------------------------------------
+        SCROLL NAV BACKGROUND ACTIVE
+    ------------------------------------*/
+
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() > 70) {
+            $('.site-navigation').addClass('header-white');
+        } else {
+            $('.site-navigation').removeClass('header-white');
+        }
+    });
+
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() > 70) {
+            $(' .top-nav').addClass('header-white-top');
+        } else {
+            $(' .top-nav').removeClass('header-white-top');
+        }
+    });
+
+    $(".gallery-item").owlCarousel({
+        items: 4,
+        margin: 10,
+        loop: true,
+        autoplay: false,
+        nav: false,
+        dots: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        mouseDrag: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 2,
+            },
+            1000: {
+                items: 4,
+            }
+        }
+    });
+
+    // Collapse navbar on click
+
+    $(document).on('click.nav', '.navbar-collapse.in', function (e) {
+        if ($(e.target).is('a')) {
+            $(this).removeClass('in').addClass('collapse');
+        }
+    });
+
+    jQuery(document).on('ready', function () {
+        /* Closes the Responsive Menu on Menu Item Click*/
+        $('.navbar-collapse ul li a').on('click', function () {
+            $('.navbar-toggler:visible').click();
+        });
+    });
+
+
+    //    Main slider
+
+    $('.carousel').carousel({
+        interval: 4000
+    })
+
+    /*---------------------------
+        VIdeo PLAY
+    -----------------------------*/
+    $('.videobox').venobox();
+
+    /*---------------------------
+        MENU SMOOTH SCROLL
+      -----------------------------*/
+    // scrollIt
+    $.scrollIt({
+        upKey: 38, // key code to navigate to the next section
+        downKey: 40, // key code to navigate to the previous section
+        easing: 'easing', // the easing function for animation
+        scrollTime: 600, // how long (in ms) the animation takes
+        activeClass: 'active', // class given to the active nav element
+        onPageChange: null, // function(pageIndex) that is called when page is changed
+        topOffset: -40 // offste (in px) for fixed top navigation
+    });
+
+
+    /* Testimonial Box Carousel */
+
+
+    // Home Swiper
+
+    function thmSwiperInit() {
+        // swiper slider
+        const swiperElm = document.querySelectorAll(".thm-swiper__slider");
+        swiperElm.forEach(function (swiperelm) {
+            const swiperOptions = JSON.parse(swiperelm.dataset.swiperOptions);
+            let thmSwiperSlider = new Swiper(swiperelm, swiperOptions);
+        });
+    }
+
+    $(window).on("load", function () {
+        if ($(".preloader").length) {
+            $(".preloader").fadeOut();
+        }
+        thmSwiperInit();
+    });
+
+    if ($(".wow").length) {
+        var wow = new WOW({
+          boxClass: "wow", // animated element css class (default is wow)
+          animateClass: "animated", // animation css class (default is animated)
+          mobile: true, // trigger animations on mobile devices (default is true)
+          live: true, // act on asynchronously loaded content (default is true)
+        });
+        wow.init();
+      }
+});
